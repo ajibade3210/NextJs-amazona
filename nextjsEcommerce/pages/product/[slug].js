762927c947cf28a91969957965748534267e355a
ajibade3210 <ajibade69th@gmail.com>
2022-06-08 14:57:43 +0100

    Creating Add To Cart Logic

--- nextjsEcommerce/pages/product/[slug].js
+++ nextjsEcommerce/pages/product/[slug].js
@@ -1,5 +1,6 @@
 // import { useRouter } from "next/router";
-import React from "react";
+import React, { useContext } from "react";
+import axios from "axios";
 import Layout from "../../components/Layout";
 // import data from "../../utils/data";  //switching to monogodb
 import db from "../../utils/db";
@@ -16,8 +17,10 @@ import {
 import useStyles from "../../utils/styles";
 import Image from "next/image";
 import Product from "../../models/Product";
+import { Store } from "../../utils/Store";
 
 function ProductScreen(props) {
+  const { dispatch } = useContext(Store);
   const { product } = props;
 
   const classes = useStyles();
@@ -28,6 +31,20 @@ function ProductScreen(props) {
   if (!product) {
     return <div>Product Not Found</div>;
   }
+
+  //Add to cart functionality
+  const addToCartHandler = async () => {
+    console.log("lkjhg");
+    //fetch data from db
+    const { data } = await axios.get(`/api/products/${product._id}`);
+
+    if (data.countInStock <= 0) {
+      window.alert("Sorry. Product is out of stock");
+      return;
+    }
+    dispatch({ type: "CART_ADD_ITEM", payload: { ...product, quantity: 1 } });
+  };
+
   return (
     <Layout title={product.name} description={product.description}>
       <div className={classes.section}>
@@ -95,7 +112,12 @@ function ProductScreen(props) {
                   </Grid>
                 </Grid>
               </ListItem>
-              <Button fullWidth variant="contained" color="primary">
+              <Button
+                fullWidth
+                variant="contained"
+                color="primary"
+                onClick={addToCartHandler}
+              >
                 Add to cart
               </Button>
             </List>
